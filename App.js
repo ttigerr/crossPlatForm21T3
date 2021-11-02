2697f119037d5b6e2587e27614c552d97823b351
Tiger <tiger.chalernphon15@gmail.com>
2021-11-03 09:53:54 +1100

    Add signin sing up firebase authentication

--- App.js
+++ App.js
@@ -2,27 +2,49 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import { Signup } from './components/Signup';
-import { Signin } from './components/Signin';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// components
+import { Signup } from './components/Signup'
+import { Signin } from './components/Signin'
 import { Home } from './components/Home';
-
 // firebase
-import {firebaseConfig} from './Config';
-//import * as firebase from 'firebase'
-import {initializeApp} from 'firebase/app'
+import { firebaseConfig } from './Config';
+import {initializeApp,} from 'firebase/app'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
-initializeApp(firebaseConfig)
+initializeApp( firebaseConfig)
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const SingupHandler = ( email, password ) => {
+      const auth = getAuth()
+      createUserWithEmailAndPassword( auth, emial, password )
+      .then((userCredential)=> {console.log(userCredential)}) 
+      .catch((error) => {console.log(error)})
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Signup" component={Signup}/>
-        <Stack.Screen name="Signin" component={Signin}/>
+        {/* <Stack.Screen 
+          name="Signup" 
+          component={Signup} 
+          options={{ 
+            title: 'Sign up'
+          }}
+        /> */}
+        <Stack.Screen name="Signup" options={{title: 'Sign up'}}>
+          {(props)=> <Signup {...props} handler={SingupHandler}/>}
+        </Stack.Screen>
+        <Stack.Screen 
+          name="Signin" 
+          component={Signin} 
+          options={{
+            title:'Sign in'
+          }}
+        />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
