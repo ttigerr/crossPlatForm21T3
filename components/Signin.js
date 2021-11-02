283f96bbb22681d9ebe93a90ea19988b87eb0cfc
Tiger <tiger.chalernphon15@gmail.com>
2021-11-03 09:54:31 +1100

    add sign in sign up and firebase authentication

--- components/Signin.js
+++ components/Signin.js
@@ -1,10 +1,26 @@
 import React from "react";
-import {View, Text, StyleSheet}from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
+import { AuthForm } from './AuthForm';
+import { useNavigation } from '@react-navigation/native';
+import { ThemeColours } from './ThemeColours';
 
 export function Signin( props ) {
+    const navigation = useNavigation()
     return(
-        <View>
-            <Text>Sign in</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Sign in</Text>
+            <AuthForm />
+            <Button title="Click here to sign up" onPress={ () => navigation.navigate("Signup") }/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 20,
+    },
+    text: {
+        textAlign: 'center',
+    },
+})
--- components/Signup.js
+++ components/Signup.js
@@ -1,16 +1,63 @@
-import React from "react";
-import {View, Text, StyleSheet, Button}from "react-native";
-import { useNavigation } from "@react-navigation/core";
+import React from 'react'
+import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import { ThemeColours } from './ThemeColours';
 
-export function Signup( props ) {
+export function Signup(props) {
 
-    // Move to another screen by using "useNavigation"
-    const navigation = useNavigation()
-    return(
-        <View>
-            <Text>Sign up</Text>
-            {/* Make an action to move to another screen */}
-            <Button title="Click here to Sign in" onPress={() => navigation.navigate("Signin")}/>
-        </View>
-    )
+  const navigation = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <Text>Sign up</Text>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+      <View style={styles.inner}>
+        <Text>Email</Text>
+        <TextInput style={styles.input} />
+        <Text>Password</Text>
+        <TextInput style={styles.input} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+        <Text>Already have an account?</Text>
+        <Button title="Click here to sign in" onPress={() => navigation.navigate("Signin")} />
+      </View>
+      </KeyboardAvoidingView>
+      
+    </View>
+  )
 }
+
+const styles = StyleSheet.create( {
+  input: {
+    backgroundColor: ThemeColours.cultured,
+    fontSize: 16,
+    padding: 5,
+    borderRadius: 4,
+  },
+  button: {
+    marginVertical: 15,
+    backgroundColor: ThemeColours.cerise,
+    padding: 10,
+    borderRadius: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: ThemeColours.turquoise,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: ThemeColours.cultured,
+    textAlign: 'center',
+  },
+  inner: {
+    width: 300,
+    marginBottom: 90,
+  },
+  kb: {
+    flex: 1,
+  }
+})
